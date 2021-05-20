10412076836dd7aba5cac35d64558e66d10813fe
ltg <ltg@bbangyanetworks.com>
2021-05-20 19:01:17 +0900

    threeLayout구상중

---
+++ src/components/threeLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <slot />
+    </div>
+    <div>
+      <slot />
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "threeLayout",
+};
+</script>
+
+<style lang="scss" scoped></style>
